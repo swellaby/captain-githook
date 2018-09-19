52757bf03a9796e8638a3ee13953343f18790765
calebcartwright <caleb.cartwright@outlook.com>
2018-09-18 20:26:32 -0500

    test: added unit tset for isValidConfigFileName function

--- pkg/captaingithook/config_test.go
+++ pkg/captaingithook/config_test.go
@@ -1,1 +1,48 @@
 package captaingithook
+
+import (
+	"testing"
+)
+
+func TestIsValidConfigFileNameReturnsFalseOnInvalidName(t *testing.T) {
+	fileName := "captaingithook.yml"
+	isValid := isValidConfigFileName(fileName)
+
+	if isValid == true {
+		t.Errorf("Validity was wrong for file name: %s. Expected: true, but got: %t.", fileName, isValid)
+	}
+}
+
+func TestIsValidConfigFileNameReturnsFalseOnEmptyName(t *testing.T) {
+	fileName := ""
+	isValid := isValidConfigFileName(fileName)
+
+	if isValid == true {
+		t.Errorf("Validity was wrong for file name: %s. Expected: true, but got: %t.", fileName, isValid)
+	}
+}
+
+func TestIsValidConfigFileNameReturnsTrueOnValidNames(t *testing.T) {
+	validConfigFileNames := []string{
+		"captaingithook.json",
+		".captaingithook.json",
+		"captaingithookrc",
+		".captaingithookrc",
+		"captaingithookrc.json",
+		".captaingithookrc.json",
+		"captain-githook.json",
+		".captain-githook.json",
+		"captain-githookrc",
+		".captain-githookrc",
+		"captain-githookrc.json",
+		".captain-githookrc.json",
+	}
+
+	for _, fileName := range validConfigFileNames {
+		isValid := isValidConfigFileName(fileName)
+
+		if isValid == false {
+			t.Errorf("Validity was wrong for file name: %s. Expected: true, but got: %t.", fileName, isValid)
+		}
+	}
+}
