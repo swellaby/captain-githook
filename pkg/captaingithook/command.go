605f2d1e183ba6db7fe53924ad3c09ff114e7734
calebcartwright <caleb.cartwright@outlook.com>
2018-09-26 18:21:07 -0500

    test: added unit tests for command functions along with some refactoring

--- pkg/captaingithook/command.go
+++ pkg/captaingithook/command.go
@@ -6,8 +6,13 @@ import (
 	"runtime"
 )
 
-var createCommand = exec.Command
+var osCommand = exec.Command
+var createCommand = newCommand
 var runCommand = run
+
+type command interface {
+	CombinedOutput() ([]byte, error)
+}
 
 func getRunnerInfo(operatingSystem string) (runner, runnerArg string) {
 	if operatingSystem == "windows" {
@@ -21,14 +26,19 @@ func getRunnerInfo(operatingSystem string) (runner, runnerArg string) {
 	return runner, runnerArg
 }
 
-func run(command string, directory string) (resultOutput string, err error) {
-	runner, runnerArg := getRunnerInfo(runtime.GOOS)
-
-	cmd := createCommand(runner, runnerArg, command)
+func newCommand(directory, name string, args ...string) command {
+	cmd := osCommand(name, args...)
 
 	if len(directory) > 0 {
 		cmd.Dir = directory
 	}
+
+	return cmd
+}
+
+func run(command string, directory string) (resultOutput string, err error) {
+	runner, runnerArg := getRunnerInfo(runtime.GOOS)
+	cmd := createCommand(directory, runner, runnerArg, command)
 
 	out, err := cmd.CombinedOutput()
 	resultOutput = string(out[:len(out)-1])
--- pkg/captaingithook/command_test.go
+++ pkg/captaingithook/command_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-type CommandRunner interface {
-	Run() string
-}
+type cmd struct {}
+
+// func (c *cmd) CombinedOutput() ()
 
 func TestGetRunnerInfoReturnsCorrectValueOnWindows(t *testing.T) {
 	const expectedRunner = "cmd.exe"
@@ -45,9 +45,54 @@ func TestGetRunnerInfoReturnsCorrectValueOnNonWindows(t *testing.T) {
 	}
 }
 
-func TestRunSetsDirectoryWhenSpecified(t *testing.T) {
-	createCommand = func(name string, arg ...string) *exec.Cmd {
-		return nil
+func TestNewCommandUsesDirectoryWhenSpecified(t *testing.T) {
+	dir := "/usr/some/other/directory"
+	mockCmd := &exec.Cmd{}
+	osCommand = func(name string, arg ...string) *exec.Cmd {
+		mockCmd.Path = name
+		mockCmd.Args = append([]string{name}, arg...)
+		return mockCmd
 	}
-	defer func() { createCommand = exec.Command }()
+	name, cmdSwitch := "sh", "-c"
+	arg := []string{ cmdSwitch, "echo", "foobar" }
+	cmd := newCommand(dir, name, arg...)
+	defer func() { osCommand = exec.Command }()
+
+	if mockCmd.Dir != dir {
+		t.Errorf("Target directory for command was incorrect. Expected: %s, but got: %s.", dir, mockCmd.Dir)
+	}
+
+	if cmd == nil {
+		t.Errorf("Got a nil exec.Command object.")
+	}
 }
+
+func TestNewCommandUsesCallingProcDirectoryWhenNotSpecified(t *testing.T) {
+	dir := ""
+	mockCmd := &exec.Cmd{}
+	osCommand = func(name string, arg ...string) *exec.Cmd {
+		mockCmd.Path = name
+		mockCmd.Args = append([]string{name}, arg...)
+		return mockCmd
+	}
+	name, cmdSwitch := "sh", "-c"
+	arg := []string{ cmdSwitch, "echo", "foobar" }
+	cmd := newCommand(dir, name, arg...)
+	defer func() { osCommand = exec.Command }()
+
+	if mockCmd.Dir != dir {
+		t.Errorf("Target directory for command was incorrect. Expected: %s, but got: %s.", dir, mockCmd.Dir)
+	}
+
+	if cmd == nil {
+		t.Errorf("Got a nil exec.Command object.")
+	}
+}
+
+// func TestRunSetsDirectoryWhenSpecified(t *testing.T) {
+// 	mockCmd := &command{}
+// 	createCommand = func(name string, arg ...string) *exec.Cmd {
+// 		return nil
+// 	}
+// 	defer func() { createCommand = exec.Command }()
+// }
