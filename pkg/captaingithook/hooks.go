760cfb6cd8c758aa8717ba2e2dd72bc5542f0842
calebcartwright <caleb.cartwright@outlook.com>
2018-10-05 19:15:00 -0500

    feat: added first part of hooks generation functionality

--- pkg/captaingithook/hooks.go
+++ pkg/captaingithook/hooks.go
@@ -1,14 +1,52 @@
 package captaingithook
 
-import ()
+import (
+	"path/filepath"
+)
 
-const hookFileContents = `#!/bin/sh
+var gitHooks = [...]string{
+	"applypatch-msg",
+	"pre-applypatch",
+	"post-applypatch",
+	"pre-commit",
+	"prepare-commit-msg",
+	"commit-msg",
+	"post-commit",
+	"pre-rebase",
+	"post-checkout",
+	"post-merge",
+	"pre-push",
+	"pre-receive",
+	"update",
+	"post-receive",
+	"post-update",
+	"push-to-checkout",
+	"pre-auto-gc",
+	"post-rewrite",
+	"sendemail-validate",
+}
+
+const hookFileScript = `#!/bin/sh
 # captain-githook
+# version v0.0.1
 
-hookName=` + "`basename \"$0\"`"
+hookName=` + "`basename \"$0\"`" + `
+gitParams="$*"
 
-func writeAllHookFiles() {
+if command -v captain-githook >/dev/null 2>&1; then
+  captain-githook $hookName "$gitParams"
+else
+  echo "Can't find captain-githook, skipping $hookName hook"
+  echo "You can reinstall it using 'go get -u github.com/swellaby/captain-githook' or delete this hook"
+fi`
 
+var hookFileContents = []byte(hookFileScript)
+
+func writeAllHookFiles(gitHooksDirectoryPath string) error {
+	firstHook := gitHooks[0]
+	hookPath := filepath.Join(gitHooksDirectoryPath, firstHook)
+	writeFile(hookPath, hookFileContents)
+	return nil
 }
 
 func removeAllHookFiles() {
--- pkg/captaingithook/hooks_test.go
+++ pkg/captaingithook/hooks_test.go
@@ -1,1 +1,38 @@
 package captaingithook
+
+import (
+	"testing"
+)
+
+var expGitHooks = [...]string{
+	"applypatch-msg",
+	"pre-applypatch",
+	"post-applypatch",
+	"pre-commit",
+	"prepare-commit-msg",
+	"commit-msg",
+	"post-commit",
+	"pre-rebase",
+	"post-checkout",
+	"post-merge",
+	"pre-push",
+	"pre-receive",
+	"update",
+	"post-receive",
+	"post-update",
+	"push-to-checkout",
+	"pre-auto-gc",
+	"post-rewrite",
+	"sendemail-validate",
+}
+
+func TestWriteAllHookFiles(t *testing.T) {
+	// var actHookPaths []string
+	originalWriteFile := writeFile
+	defer func() { writeFile = originalWriteFile }()
+	writeFile = func(filePath string, contents []byte) error {
+		// actHookPaths.
+		return nil
+	}
+	writeAllHookFiles("")
+}
