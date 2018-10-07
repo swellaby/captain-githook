a7d9187340164d4f27190f45046ead4d0cc26c50
calebcartwright <caleb.cartwright@outlook.com>
2018-10-06 19:01:58 -0500

    chore: finished switching from Mage to Task as part of #21

--- scripts/setup.go
+++ scripts/setup.go
@@ -9,27 +9,27 @@ import (
 	"runtime"
 )
 
-func installMage() {
-	cmd := exec.Command("go", "get", "github.com/magefile/mage")
+func installTask() {
+	cmd := exec.Command("go", "get", "-v", "github.com/go-task/task/cmd/task")
 	cmd.Dir = os.TempDir()
 	out, err := cmd.CombinedOutput()
 
 	if err != nil {
-		fmt.Printf("An error occurred while installing mage %s\n", err)
+		fmt.Printf("An error occurred while installing task %s\n", err)
 		fmt.Printf("Error details: %s\n", string(out))
 		os.Exit(1)
 	} else {
-		fmt.Println("Mage successfully installed.")
+		fmt.Println("Task successfully installed.")
 	}
 }
 
-func runMageSetupTarget() {
-	cmd := exec.Command("mage", "setup")
+func runTaskSetupTarget() {
+	cmd := exec.Command("task", "setup")
 	_, currentFilePath, _, _ := runtime.Caller(0)
 	cmd.Dir = filepath.Join(path.Dir(currentFilePath), "..")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Error encountered while running `mage setup`. Error details: %s\n", err)
+		fmt.Printf("Error encountered while running `task setup`. Error details: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("%s", string(out))
@@ -37,8 +37,8 @@ func runMageSetupTarget() {
 }
 
 func main() {
-	fmt.Println("Ensuring Mage is installed and available...")
-	installMage()
-	fmt.Println("Running `mage setup` to configure workspace...")
-	runMageSetupTarget()
+	fmt.Println("Ensuring Task is installed and available...")
+	installTask()
+	fmt.Println("Running `task setup` to configure workspace...")
+	runTaskSetupTarget()
 }
